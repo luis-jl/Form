9d9093bf1d9bd1a10aa9d912c7499885f4e8dbdd
Luis JL <luisjl80456@gmail.com>
2022-10-14 20:35:13 +0200

    comit inicial

---
+++ app.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded",function() {
+    const formulario = document.querySelector("#formulario");
+    const inputEmail = document.querySelector("#email");
+    const inputAsunto = document.querySelector("#asunto");
+    const textArea = document.querySelector("textArea");
+
+});
+
